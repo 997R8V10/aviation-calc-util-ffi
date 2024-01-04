a97df62d0f645f3a127040c4dde7274cacb451fe
Prithvi Shivaraman <prithvisagar.shivaraman@gmail.com>
2024-01-04 01:40:02 -0500

    Added no mangle

--- src/physics/mod.rs
+++ src/physics/mod.rs
@@ -5,6 +5,7 @@ use crate::interop::{InteropDateTimeStruct, struct_to_rust_duration, rust_durati
 /// Calculates Final Velocity from Initial Velocity and Time
 /// 
 /// Kinematics Equation: V<sub>f</sub> = V<sub>i</sub> + a * t
+#[no_mangle]
 pub unsafe extern fn physics_kinematics_final_velocity(initial_velocity: *mut Velocity, acceleration: *mut Acceleration, time: InteropDateTimeStruct) -> *mut Velocity {
     return Box::leak(Box::new(physics::kinematics_final_velocity(*initial_velocity, *acceleration, struct_to_rust_duration(time))));
 }
@@ -14,6 +15,7 @@ pub unsafe extern fn physics_kinematics_final_velocity(initial_velocity: *mut Ve
 /// Kinematics Equation: V<sub>f</sub> = V<sub>i</sub> + a * t
 /// 
 /// **Note**: All Units must be kept constant!
+#[no_mangle]
 pub unsafe extern fn physics_kinematics_final_velocity_general(initial_velocity: f64, acceleration: f64, time: f64) -> f64 {
     return physics::kinematics_final_velocity_general(initial_velocity, acceleration, time);
 }
@@ -21,6 +23,7 @@ pub unsafe extern fn physics_kinematics_final_velocity_general(initial_velocity:
 /// Calculates Displacement from Initial Velocity, Final Velocity, and Time
 /// 
 /// Kinematics Equation: Δx = ((V<sub>i</sub> + V<sub>f</sub>)/2) * t
+#[no_mangle]
 pub unsafe extern fn physics_kinematics_displacement_1(initial_velocity: *mut Velocity, final_velocity: *mut Velocity, time: InteropDateTimeStruct) -> *mut Length {
     return Box::leak(Box::new(physics::kinematics_displacement_1(*initial_velocity, *final_velocity, struct_to_rust_duration(time))));
 }
@@ -31,6 +34,7 @@ pub unsafe extern fn physics_kinematics_displacement_1(initial_velocity: *mut Ve
 /// Kinematics Equation: Δx = ((V<sub>i</sub> + V<sub>f</sub>)/2) * t
 /// 
 /// **Note**: All Units must be kept constant!
+#[no_mangle]
 pub unsafe extern fn physics_kinematics_displacement_1_general(initial_velocity: f64, final_velocity: f64, time: f64) -> f64 {
     return physics::kinematics_displacement_1_general(initial_velocity, final_velocity, time);
 }
@@ -38,6 +42,7 @@ pub unsafe extern fn physics_kinematics_displacement_1_general(initial_velocity:
 /// Calculates Displacement from Initial Velocity, Acceleration, and Time
 /// 
 /// Kinematics Equation: Δx = V<sub>i</sub> * t + (1/2) * a * t<sup>2</sup>
+#[no_mangle]
 pub unsafe extern fn physics_kinematics_displacement_2(initial_velocity: *mut Velocity, acceleration: *mut Acceleration, time: InteropDateTimeStruct) -> *mut Length {
     return Box::leak(Box::new(physics::kinematics_displacement_2(*initial_velocity, *acceleration, struct_to_rust_duration(time))));
 }
@@ -48,6 +53,7 @@ pub unsafe extern fn physics_kinematics_displacement_2(initial_velocity: *mut Ve
 /// Kinematics Equation: Δx = V<sub>i</sub> * t + (1/2) * a * t<sup>2</sup>
 /// 
 /// **Note**: All Units must be kept constant!
+#[no_mangle]
 pub unsafe extern fn physics_kinematics_displacement_2_general(initial_velocity: f64, acceleration: f64, time: f64) -> f64 {
     return physics::kinematics_displacement_2_general(initial_velocity, acceleration, time);
 }
@@ -55,6 +61,7 @@ pub unsafe extern fn physics_kinematics_displacement_2_general(initial_velocity:
 /// Calculates Time from Displacement, Initial Velocity, and Final Velocity.
 /// 
 /// Kinematics Equation: t = 2 * Δx / (V<sub>i</sub> + V<sub>f</sub>)
+#[no_mangle]
 pub unsafe extern fn physics_kinematics_time_1(displacement: *mut Length, initial_velocity: *mut Velocity, final_velocity: *mut Velocity) -> InteropDateTimeStruct {
     return rust_duration_to_struct(physics::kinematics_time_1(*displacement, *initial_velocity, *final_velocity));
 }
@@ -64,6 +71,7 @@ pub unsafe extern fn physics_kinematics_time_1(displacement: *mut Length, initia
 /// Kinematics Equation: t = 2 * Δx / (V<sub>i</sub> + V<sub>f</sub>)
 /// 
 /// **Note**: All Units must be kept constant!
+#[no_mangle]
 pub unsafe extern fn physics_kinematics_time_1_general(displacement: f64, initial_velocity: f64, final_velocity: f64) -> f64 {
     return physics::kinematics_time_1_general(displacement, initial_velocity, final_velocity);
 }
